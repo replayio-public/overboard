a832a31b33a7f7c45676580fa2f06b71b42a5330
Travis Arnold <travis@replay.io>
2022-06-08 15:56:05 -0700

    don't minify component names

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+import WebpackReactComponentNamePlugin from 'webpack-react-component-name'
+
 export default {
   reactStrictMode: true,
   productionBrowserSourceMaps: true,
+  webpack(config) {
+    config.plugins.push(new WebpackReactComponentNamePlugin())
+    return config
+  },
 }
