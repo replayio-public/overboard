f0f5cbc21a21d98e0257b045a2d4bb83a4bf3fee
Travis Arnold <travis@replay.io>
2022-08-04 14:05:14 -0700

    move back to webpack-react-component-name plugin

--- packages/site/next.config.mjs
+++ packages/site/next.config.mjs
@@ -1,7 +1,14 @@
-export default {
+import withTM from "next-transpile-modules";
+import WebpackReactComponentNamePlugin from "webpack-react-component-name";
+
+export default withTM(["@replayio/overboard"])({
   productionBrowserSourceMaps: true,
   reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: true,
   },
-};
+  webpack(config) {
+    config.plugins.push(new WebpackReactComponentNamePlugin());
+    return config;
+  },
+});
