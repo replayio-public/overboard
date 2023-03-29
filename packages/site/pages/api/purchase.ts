768ee53d819071bffaa33c18952c3777c32dbca4
Tejas Kumar <tejas@tejas.qa>
2023-03-29 16:02:49 +0200

    Fix

--- packages/site/pages/api/purchase.ts
+++ packages/site/pages/api/purchase.ts
@@ -1,15 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type RequestData = { colorId: string };
+type RequestData = { color: string };
 
 type ResponseData = { message: string };
 
 export default (request: NextApiRequest, response: NextApiResponse<ResponseData>) => {
   const body = request.body as RequestData;
 
-  if (!body.colorId) {
-    return response.status(400).json({ message: `Color not found, received: ${body.colorId}` });
+  if (!body.color) {
+    return response.status(400).json({ message: `Color not found, received: ${body.color}` });
   }
 
-  response.status(200).json({ message: `Color "${body.colorId}" successfully purchased!` });
+  response.status(200).json({ message: `Color "${body.color}" successfully purchased!` });
 };
