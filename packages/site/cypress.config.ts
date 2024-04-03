fee078dca50baa93c96bd89df9246c100fcabbb7
Jason Laster <jason.laster.11@gmail.com>
2024-04-03 10:25:31 -0700

    Update packages/site/cypress.config.ts
    Co-authored-by: Mateusz Burzyński <mateuszburzynski@gmail.com>

--- packages/site/cypress.config.ts
+++ packages/site/cypress.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "cypress";
-const { plugin: replayPlugin } = require("@replayio/cypress");
+import { plugin as replayPlugin } from "@replayio/cypress";
 
 export default defineConfig({
   e2e: {
